0f8b902369ee7df80113801fe7171f08f865ef1a
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-04-07 14:08:02 +0600

    design improved loading added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,36 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
+import LoadSpin from '../LoadSpin/LoadSpin';
 import './Home.css'
-import transports from '../../fakedata.json'
+
 
 
 const Home = () => {
     const [fruits, setFruits] = useState([]);
 
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('http://localhost:5000/products')
-        .then(response => response.json())
-        .then(data =>{
-            setFruits(data)
-            console.log(data);
-        });
-    },[])
+            .then(response => response.json())
+            .then(data => {
+                setFruits(data)
+                console.log(data);
+            });
+    }, [])
     return (
-
-     
-        <div className="bg">   
-                <div  className="row me-5 ms-5 customPadding">
-                {
-                    fruits.map(fruit => {
-                        return <Card fruit={fruit}>
-                        </Card>
-                    })
-                }
+        fruits[0] ?
+            <div className="bg">
+                <div className="row me-5 ms-5 customPadding">
+                    {
+                        fruits.map(fruit => {
+                            return <Card fruit={fruit}>
+                            </Card>
+                        })
+                    }
+                </div>
             </div>
-          
+            :
+                <div className="center center-text">
+                    <LoadSpin />
+                </div>
 
-        </div>
+
+
     );
 };
 
